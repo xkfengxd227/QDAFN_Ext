6a83acad3a33d0560f3c232d04acd320e46ab294
Qiang Huang <huangq2011@gmail.com>
2018-05-03 23:54:41 +0800

    update file path

--- run_heuristic.sh
+++ run_heuristic.sh
@@ -15,8 +15,8 @@ dname=Mnist
 n=59000
 d=50
 B=4096
-dPath=../../data/${dname}/${dname}
-dFolder=../../data/${dname}/
+dPath=./data/${dname}/${dname}
+dFolder=./data/${dname}/
 
 proj=10
 for ((i=2; i<=10; i=i+1))
@@ -25,7 +25,7 @@ do
   cand=$((273 - $proj))	
   for ((j=1; j<=5; j=j+1))
   do
-	oFolder=../../results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
+	oFolder=./results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
 
 	./qdafn -alg 1 -n ${n} -d ${d} -B ${B} -l ${proj} -m ${cand} -c ${c} -ds ${dPath}.ds -df ${dFolder} -of ${oFolder}
 	./qdafn -alg 2 -qn ${qn} -d ${d} -qs ${dPath}.q -ts ${dPath}.fn2.0 -df ${dFolder} -of ${oFolder}
@@ -39,15 +39,15 @@ dname=Sift
 n=999000
 d=128
 B=4096
-dPath=../../data/${dname}/${dname}
-dFolder=../../data/${dname}/
+dPath=./data/${dname}/${dname}
+dFolder=./data/${dname}/
 
 cand=122
 for proj in 50 60 70 80 90 100
 do
   for ((j=1; j<=5; j=j+1))
   do
-    oFolder=../../results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
+    oFolder=./results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
 
 	./qdafn -alg 1 -n ${n} -d ${d} -B ${B} -l ${proj} -m ${cand} -c ${c} -ds ${dPath}.ds -df ${dFolder} -of ${oFolder}
 	./qdafn -alg 2 -qn ${qn} -d ${d} -qs ${dPath}.q -ts ${dPath}.fn2.0 -df ${dFolder} -of ${oFolder}
@@ -61,15 +61,15 @@ dname=Gist
 n=999000
 d=960
 B=16384
-dPath=../../data/${dname}/${dname}
-dFolder=../../data/${dname}/
+dPath=./data/${dname}/${dname}
+dFolder=./data/${dname}/
 
 cand=21
 for proj in 60 80 100
 do
   for ((j=1; j<=10; j=j+1))
   do
-	oFolder=../../results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
+	oFolder=./results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
 
 	./qdafn -alg 1 -n ${n} -d ${d} -B ${B} -l ${proj} -m ${cand} -c ${c} -ds ${dPath}.ds -df ${dFolder} -of ${oFolder}
 	./qdafn -alg 2 -qn ${qn} -d ${d} -qs ${dPath}.q -ts ${dPath}.fn2.0 -df ${dFolder} -of ${oFolder}
@@ -83,8 +83,8 @@ dname=Trevi
 n=99900
 d=4096
 B=65536
-dPath=../../data/${dname}/${dname}
-dFolder=../../data/${dname}/
+dPath=./data/${dname}/${dname}
+dFolder=./data/${dname}/
 
 proj=10
 cand=27
@@ -93,7 +93,7 @@ do
   proj=$(($proj + 10))
   for ((j=1; j<=5; j=j+1))
   do
-	oFolder=../../results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
+	oFolder=./results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
 
 	./qdafn -alg 1 -n ${n} -d ${d} -B ${B} -l ${proj} -m ${cand} -c ${c} -ds ${dPath}.ds -df ${dFolder} -of ${oFolder}
 	./qdafn -alg 2 -qn ${qn} -d ${d} -qs ${dPath}.q -ts ${dPath}.fn2.0 -df ${dFolder} -of ${oFolder}
@@ -107,8 +107,8 @@ dname=P53
 n=30159
 d=5408
 B=65536
-dPath=../../data/${dname}/${dname}
-dFolder=../../data/${dname}/
+dPath=./data/${dname}/${dname}
+dFolder=./data/${dname}/
 
 proj=10
 cand=21
@@ -117,7 +117,7 @@ do
   proj=$(($proj + 10))
   for ((j=1; j<=5; j=j+1))
   do
-	oFolder=../../results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
+	oFolder=./results${c}/${dname}/qdafn/heuristic/${proj}/${j}/
 
 	./qdafn -alg 1 -n ${n} -d ${d} -B ${B} -l ${proj} -m ${cand} -c ${c} -ds ${dPath}.ds -df ${dFolder} -of ${oFolder}
 	./qdafn -alg 2 -qn ${qn} -d ${d} -qs ${dPath}.q -ts ${dPath}.fn2.0 -df ${dFolder} -of ${oFolder}
